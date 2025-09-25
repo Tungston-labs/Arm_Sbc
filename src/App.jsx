3254d0465161cf0add5cd0f83eee8916cfdd3379
Tungston-labs <support@tungstonlabs.com>
2025-09-25 17:24:36 +0530

    no changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Comparison from './pages/Comparison/Comparison'
 import Navbar from './Components/Navbar/Navbar'
 
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import SingleProduct from './pages/product/SingleProduct.jsx'
 // import Navbar from './Components/Navbar/Navbar'
 import CartPage from './pages/cartpage/CartPage'
@@ -16,7 +16,7 @@ function App() {
   <Route path='/nav' element={<Navbar/>}/>
 <Route path="/compare" element={<Comparison/>}/>
   <Route path='/cartpage' element={<CartPage/>}/>
-<Route path='/' element={<Home/>}/>
+{/* <Route path='/' element={<Home/>}/> */}
 <Route path='/product/:id' element={<SingleProduct/>}/>
 
 </Routes>

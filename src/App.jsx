42b175df5f2a538126d9c468abd28074cfd22d4e
Tungston-labs <support@tungstonlabs.com>
2025-09-25 17:29:01 +0530

    new commit in feature/integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Comparison from './pages/Comparison/Comparison'
 import Navbar from './Components/Navbar/Navbar'
-<<<<<<< HEAD
-
-// import Home from './pages/Home'
-=======
->>>>>>> a9b5d805e4b3115c4b667e61b6e3f882113d1bfc
 import SingleProduct from './pages/product/SingleProduct.jsx'
 // import Navbar from './Components/Navbar/Navbar'
 import CartPage from './pages/cartpage/CartPage'

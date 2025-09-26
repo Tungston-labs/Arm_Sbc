797a8e7a8b1b8fcfa823264e59af93eeac1992e4
Tungston-labs <jesna@tungstonlabs.com>
2025-09-26 15:13:38 +0530

    conflict

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,11 @@ import Verification from "./pages/Login/Verification";
 import SetNewPassword from "./pages/Login/SetNewPassword";
 import Comparison from './pages/Comparison/Comparison'
 import SingleProduct from './pages/product/SingleProduct.jsx'
-import CartPage from './pages/cartpage/CartPage'
 import Dashboard from './pages/dashboard/Dashboard'
 import Enquiry from './pages/EnquiryPage/Enquiry'
 import EnquiryDetails from './pages/EnquiryPage/EnquiryDetails'
 import InquiryPage from './pages/Inquiryform/InquiryPage'
-import ChangePasswordModal from './Components/ChangePasswordModal/ChangePasswordModal.jsx'
+
 
 function App() {
   return (
@@ -27,7 +26,6 @@ function App() {
 <Route path='/dashboard' element={<Dashboard/>}/>
 <Route path='/enquiry-page' element={<Enquiry/>}/>
 <Route path="/enquiry-details/:id" element={<EnquiryDetails />} />
-<Route path="/password" element={<ChangePasswordModal />} />
  <Route path="/login" element={<Login />} />
       <Route path="/reset-password" element={<Resetpassword />} />
       <Route path="/verification" element={<Verification />} />

bd2756c3754963eac00d4f2a80e6562d1e7ed317
Tungston-labs <support@tungstonlabs.com>
2025-09-26 17:48:57 +0530

    api integration home screen

--- src/Components/ProductCard/ProductSection.jsx
+++ src/Components/ProductCard/ProductSection.jsx
@@ -7,12 +7,16 @@ import {
 } from "./ProductSection.style";
 
 const ProductSection = ({ products }) => {
+  if (!products || products.length === 0) {
+    return <p>No products available</p>;
+  }
+
   return (
     <ProductsWrapper>
       {products.map((product) => (
         <Card key={product.id}>
-          <ProductImage src={product.image} alt={product.title} />
-          <ProductTitle>{product.title}</ProductTitle>
+          <ProductImage src={product.image} alt={product.name} />
+          <ProductTitle>{product.ram} {product.storage}</ProductTitle>
         </Card>
       ))}
     </ProductsWrapper>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import store from "./redux/store"
+import { Provider } from "react-redux"; 
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>         
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </Provider>
 );

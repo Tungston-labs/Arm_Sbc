4ba6dadff580cfe1fdfdc095c1756c3e2e30557a
Tungston-labs <tungstonlabs@gmail.com>
2025-09-27 09:52:36 +0530

    Login API integration completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { refreshToken } from "./useRefreshTokenService";
 const api = axios.create({
   baseURL: "http://178.248.112.16:8002/api/", 
   headers: {
@@ -8,11 +8,27 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token"); 
+  const token = localStorage.getItem("access_token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const prevRequest = error.config;
+    if (error.response?.status === 401 && !prevRequest._retry) {
+      prevRequest._retry = true;
+      const newToken = await refreshToken();
+      if (newToken) {
+        prevRequest.headers.Authorization = `Bearer ${newToken}`;
+        return api(prevRequest);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;

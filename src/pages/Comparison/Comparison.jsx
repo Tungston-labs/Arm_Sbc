be5e9a6ab5d056523475fedcfd96a6ea82e753a4
Tungston-labs <tungstonlabs@gmail.com>
2025-10-01 18:24:05 +0530

    Home page ui and alignement is fixed

--- src/pages/Comparison/Comparison.jsx
+++ src/pages/Comparison/Comparison.jsx
@@ -89,7 +89,7 @@ const Comparison = () => {
     <Main>
       <Navbar />
       <Container>
-        <Header>// ✅ navigate on click
+        <Header>
           <h1>Comparison </h1>
           <Button onClick={handleAddProduct}>Compare Product</Button>
         </Header>
